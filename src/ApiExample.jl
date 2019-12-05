31946cfa0e1f59bca8269b46183be6b75b356186
Matt Helm <mthelm85@gmail.com>
2019-12-05 14:44:45 -0600

    Files generated by PkgTemplates

---
+++ src/ApiExample.jl
@@ -0,0 +1,5 @@
+module ApiExample
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ApiExample
+using Test
+
+@testset "ApiExample.jl" begin
+    # Write your own tests here.
+end
